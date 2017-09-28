45ee3fbf1a714737a3597d9fe2c32805b2df9ea4
victor <victor-gr7@hotmail.com>
2017-09-29 01:34:38 +0200

    Initial Commit

---
+++ Práctica2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Práctica2
+//
+//  Created by victor on 29/9/17.
+//  Copyright © 2017 UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
